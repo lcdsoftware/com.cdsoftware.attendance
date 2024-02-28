1e3966446c78f5b1ab02933616a678af01fafb97
alara <alara@ghintech.com>
2024-02-28 18:03:38 -0500

    Agregar annotations en callouts

--- com.cdsoftware.lirion.attendance/src/com/cdsoftware/lirion/attendance/callout/CheckBpPermission.java
+++ com.cdsoftware.lirion.attendance/src/com/cdsoftware/lirion/attendance/callout/CheckBpPermission.java
@@ -3,6 +3,8 @@ package com.cdsoftware.lirion.attendance.callout;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.Properties;
+
+import org.adempiere.base.annotation.Callout;
 import org.compiere.model.GridField;
 import org.compiere.model.GridTab;
 import org.compiere.model.Query;
@@ -12,6 +14,7 @@ import org.compiere.util.Msg;
 
 import com.cdsoftware.lirion.attendance.base.CustomCallout;
 
+@Callout(tableName = "MRequest", columnName = {"CDS_R_RequestTypeDetails_ID","R_RequestType_ID","C_BPartner_ID","CDS_StartDate"})
 public class CheckBpPermission extends CustomCallout{
 	protected CLogger			log = CLogger.getCLogger (getClass());
 	@Override
