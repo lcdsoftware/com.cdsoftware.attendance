2fcdd02e605ed59ff6dcd9d190646fe10aa15c8b
carlo <carlogonzalez@casadelsoftware.com>
2024-11-19 18:11:47 -0300

    Update process

--- com.cdsoftware.lirion.attendance/src/com/cdsoftware/lirion/attendance/process/ImportAttendanceFromI_Attendance.java
+++ com.cdsoftware.lirion.attendance/src/com/cdsoftware/lirion/attendance/process/ImportAttendanceFromI_Attendance.java
@@ -243,9 +243,14 @@ public class ImportAttendanceFromI_Attendance extends CustomProcess {
 
 
 		endDate = parsedDate;
-		attendance.setDateFrom(startDate);
-		attendance.setDateTo(endDate);
-		attendance.setName(startDate.toString().concat(" to ").concat(endDate.toString()));
+		//attendance.setDateFrom(startDate);
+		//attendance.setDateTo(endDate);
+		//attendance.setName(startDate.toString().concat(" to ").concat(endDate.toString()));
+		String startDateStr = (startDate != null) ? startDate.toString() : "N/A";
+		String endDateStr = (endDate != null) ? endDate.toString() : "N/A";
+
+		attendance.setName(startDateStr.concat(" to ").concat(endDateStr));
+
 		attendance.saveEx();
 		return null;
 	}
